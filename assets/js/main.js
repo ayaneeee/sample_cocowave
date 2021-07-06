b3fbef702ab36506046ef6c4427bd22a4b5d47f5
下里彩音 <harmys10@gmail.com>
2021-07-06 13:52:47 +0900

    20210706

---
+++ assets/js/main.js
@@ -0,0 +1,81 @@
+$(function () {
+
+  //Loading
+  $(window).on('load',function(){
+    $("#splash").delay(1500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
+    $("#splash_logo").delay(1200).fadeOut('slow');//ロゴを1.2秒（1200ms）待機してからフェードアウト
+  });
+  // ハンバーガー
+
+    $(function() {
+        $('.drawer').drawer();
+    });
+
+
+  // main画面 スライダー
+  $('.bgimg-slider').bgSwitcher({
+    images: ['./assets/img/beach.jpeg', './assets/img/livingroom2.jpeg', './assets/img/out.jpg'],
+    effect: "fade",
+    easing: "linear"
+  });
+// slick
+
+  $(function () {
+    $('.room_gallery').slick({
+      autoplay: true,
+      autoplaySpeed: 2000,
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          }
+        }
+      ]
+    });
+  });
+  const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_v7fj61j';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
+});
+
+
